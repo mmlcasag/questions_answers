2e565dd0d3b3de478dd40f10863dab628c8ffa72
Márcio Luís Casagrande <mmlcasag@gmail.com>
2021-05-03 20:34:21 -0300

    Database Helpers

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from database import get_db, close_db
 
 app = Flask(__name__)
 
---
+++ database.py
@@ -0,0 +1,16 @@
+from flask import g
+import sqlite3
+
+def connect_db():
+    sql = sqlite3.connect('/home/mmlcasag/python/questions_answers/database/questions_answers.db')
+    sql.row_factory = sqlite3.Row
+    return sql
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+def close_db():
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
